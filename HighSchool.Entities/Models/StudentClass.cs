edeaa5925dc26cac6e3ba9228bfdeb23fcc786a9
SmartGanyaupfu <smartganyaupfu@gmail.com>
2023-04-03 18:42:20 +0200

    Added New DB Tables

---
+++ HighSchool.Entities/Models/StudentClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace HighSchool.Entities.Models
+{
+    public class Grade:BaseEntity
+    {
+        public int GradeId { get; set; }
+        public string? Name { get; set; }
+        public int StudentLevelId { get; set; }
+        public StudentLevel? StudentLevel { get; set; }
+        // public string? Level { get; set; }
+        //  public DateTime? Year { get; set; }
+        // public Guid StaffId { get; set; }
+        // public Staff? ClassTeacher { get; set; }
+        // public ICollection<StudentGrade>? StudentGrades { get; set; }
+
+
+
+    }
+}
+
